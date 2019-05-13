0d038d3cf356bd9d1475177d92187b1f4e91a0b7
gkdl2197 <50482862+gkdl2197@users.noreply.github.com>
2019-05-13 11:19:35 +0900

    Update MainActivity.java
    setText 추가

--- app/src/main/java/edu/android/and47_git/MainActivity.java
+++ app/src/main/java/edu/android/and47_git/MainActivity.java
@@ -16,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
+        textView.setText("My First GitHub Project");
     }
 }
